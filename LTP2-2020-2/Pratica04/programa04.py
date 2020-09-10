b6810d9afb42a45738b0130b14206d1330481998
biellvieira <62109597+biellvieira@users.noreply.github.com>
2020-09-10 10:43:08 -0300

    Create programa04.py

---
+++ LTP2-2020-2/Pratica04/programa04.py
@@ -0,0 +1,41 @@
+#Lista vazia com as temperaturas
+#temperaturas = [23, 5, -6, 34]
+temperaturas = []
+
+
+print(temperaturas)
+
+
+#Pede para o usuario digitar 5 temperaturas
+contador = 0
+while contador < 5:
+  temperatura = float(input('Informe uma temperatura:'))
+  #Adiciona temperatura no final
+  temperaturas.append(temperatura)
+  print(temperaturas)
+  contador += 1
+
+#Encontrar o maior valor
+maior = max(temperaturas)
+print('Maior valor:', maior)
+
+#Encontra o valor menor
+menor = min(temperaturas)
+print('Menor valor:', menor)
+
+#Calcula o valor medio, pela somatoria e a contagem de elementos
+media = sum(temperaturas)/ len(temperaturas)
+print('Temperatura media:', media)
+
+#Controla o formato de exibicao da media
+print('Temperatura media: %.3f' % (media))
+
+#Coloca a lista em ordem
+temperaturas.sort()
+print('Ordenacao crescente:')
+print(temperaturas)
+
+#Ordenacao decrescente
+temperaturas.sort(reverse=True)
+print('Ordenacao decrescente:')
+print(temperaturas)
