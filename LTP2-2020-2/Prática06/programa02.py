cc6644e61b112737d1251aff002f00204ed2f15a
biellvieira <62109597+biellvieira@users.noreply.github.com>
2020-09-24 10:13:17 -0300

    Create programa02.py

---
+++ LTP2-2020-2/Prática06/programa02.py
@@ -0,0 +1,20 @@
+def exibirStatusLista(lista):
+  valor_medio = sum(lista) / len(lista)
+  maior_valor = max(lista)
+  menor_valor = min(lista)
+  lista.sort()
+  print("valor médio:", valor_medio)
+  print("maior valor:", maior_valor)
+  print("menor valor:", menor_valor)
+  print("lista ordenada:", lista)
+
+def leituraLista(lista, quantidade):
+  while len(lista) < quantidade:
+    valor = float(input("Informe um valor:"))
+    lista.append(valor)
+
+#Programa principal
+valores = []
+quantidade = int(input("Informe uma quantidade:"))
+leituraLista(valores, quantidade)
+exibirStatusLista(valores)
