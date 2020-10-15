7bbab7828043ea40574f5b1d6ccd1ab02492c304
biellvieira <62109597+biellvieira@users.noreply.github.com>
2020-10-15 09:59:09 -0300

    Create prog01.py

---
+++ LTP2-2020-2/Prática07/prog01.py
@@ -0,0 +1,11 @@
+#Cria um dicionario vazio
+
+dicionario = {}
+
+#Adiciona um valor ao docionario
+dicionario["eletrico"] = "pikachu"
+dicionario["fogo"] = "charizard"
+dicionario["agua"] = "blastoise"
+dicionario[1] = "Lalala"
+
+print(dicionario)
